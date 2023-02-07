07212143070470c85eda63b0797005b3a4580e32
dinarfathrahmanov <dinfat@mail.ru>
2023-02-07 23:03:38 +0300

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
+        task3();
     }
-    public static void task1(){
+    public static void task1() {
         System.out.println("Задача 1");
-        int age =20;
-        if (age>=18){
+        int age = 20;
+        if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + " ,то он совершеннолетний");
-        }else{
+        } else {
             System.out.println("Eсли возраст человека равен " + age + " ,он не достиг совершеннолетия, нужно немного подождать");
-
         }
     }
-}
+
+        public static void task2() {
+            System.out.println("Задача 2");
+            int c = 10;
+            if (c >= 5) {
+                System.out.println("На улице " + c + " градусов, можно идти без шапки");
+            } else {
+                System.out.println("На улице " + c + " нужно надеть шапку");
+            }
+        }
+    public static void task3() {
+        System.out.println("Задача 3");
+    }
+    }
+
