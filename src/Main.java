8f97c9f548d23550c9761eb2a33c4a96e87db7ca
dinarfathrahmanov <dinfat@mail.ru>
2023-02-07 23:15:21 +0300

    задача 3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
     }
+
     public static void task1() {
         System.out.println("Задача 1");
         int age = 20;
@@ -14,17 +15,25 @@ public class Main {
         }
     }
 
-        public static void task2() {
-            System.out.println("Задача 2");
-            int c = 10;
-            if (c >= 5) {
-                System.out.println("На улице " + c + " градусов, можно идти без шапки");
-            } else {
-                System.out.println("На улице " + c + " нужно надеть шапку");
-            }
+    public static void task2() {
+        System.out.println("Задача 2");
+        int c = 10;
+        if (c >= 5) {
+            System.out.println("На улице " + c + " градусов, можно идти без шапки");
+        } else {
+            System.out.println("На улице " + c + " нужно надеть шапку");
         }
+    }
+
     public static void task3() {
         System.out.println("Задача 3");
+        int speed = 75;
+        if (speed >= 60) {
+            System.out.println("Если скорость " + speed + " , то придется заплатить штраф");
+        } else {
+            System.out.println("Если скорость " + speed + " , можно ездить спокойно");
+        }
     }
-    }
+}
+
 
